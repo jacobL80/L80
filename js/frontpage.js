f28b6de57448a79e3f0adeb6249b1192d9022eb2
Jacob Leighty <JacobAlexander.Leighty1@T-Mobile.com>
2017-10-23 16:23:40 -0700

    added template for homepage

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
-
+    "use strict";
+    
+    
+    
+    $(document).on("mouseover", ".buttonDiv", function(e) {
+        e.preventDefault(); 
+        var id = $(this).attr("id");
+        $("img[parentId='" + id + "']").css("opacity", ".6");
+        $("div[parentId='" + id + "']").css("opacity", ".6");
+    });
+    
+    $(document).on("mouseout", ".buttonDiv", function(e) {
+        e.preventDefault(); 
+        var id = $(this).attr("id");
+        $("img[parentId='" + id + "']").css("opacity", "1");
+        $("div[parentId='" + id + "']").css("opacity", "0");
+    });
 });
