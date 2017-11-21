d6aecd0fe96c6758f5fa341694edd6584eae3c5a
Jacob Leighty <JacobAlexander.Leighty1@T-Mobile.com>
2017-11-21 14:06:16 -0800

    added about

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     "use strict";
     
-    
-    
     $(document).on("mouseover", ".buttonDiv", function(e) {
         e.preventDefault(); 
         var id = $(this).attr("id");
@@ -15,5 +13,17 @@ $(document).ready(function () {
         var id = $(this).attr("id");
         $("img[parentId='" + id + "']").css("opacity", "1");
         $("div[parentId='" + id + "']").css("opacity", "0");
+    });
+    
+    $(document).on("click", "#resumeButton", function(e) {
+        e.preventDefault();  
+        $(".resumeRow").toggle();
+        var $this = $(this);
+		$this.toggleClass('showResume');
+		if($this.hasClass('showResume')){
+			$this.text('Show Resume');			
+		} else {
+			$this.text('Hide Resume');
+		}
     });
 });
