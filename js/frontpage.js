4c53c93f6e76db979cb32cb6223266e0bc23ed1a
Jacob Leighty <JacobAlexander.Leighty1@T-Mobile.com>
2017-12-13 15:41:39 -0800

    added JSON file, fixed text opacity on hover, and added L80comics to webcomics link

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,18 +1,22 @@
+var baseURL = 'https://api.myjson.com/bins/1b5m0r';
+
 $(document).ready(function () {
     "use strict";
     
     $(document).on("mouseover", ".buttonDiv", function(e) {
         e.preventDefault(); 
         var id = $(this).attr("id");
-        $("img[parentId='" + id + "']").css("opacity", ".6");
-        $("div[parentId='" + id + "']").css("opacity", ".6");
+        $("img[parentId='" + id + "']").css("background", "rgba(0,140,186,.6)");
+        $("div[parentId='" + id + "']").css("background", "rgba(0,140,186,.6)");
+        $("div[parentId='" + id + "'] .text").css("color", "rgba(255,255,255,1)");
     });
     
     $(document).on("mouseout", ".buttonDiv", function(e) {
         e.preventDefault(); 
         var id = $(this).attr("id");
-        $("img[parentId='" + id + "']").css("opacity", "1");
-        $("div[parentId='" + id + "']").css("opacity", "0");
+        $("img[parentId='" + id + "']").css("background", "rgba(0,140,186,1)");
+        $("div[parentId='" + id + "']").css("background", "rgba(0,140,186,0)");
+        $("div[parentId='" + id + "'] .text").css("color", "rgba(255,255,255,0)");
     });
     
     $(document).on("click", "#resumeButton", function(e) {
@@ -26,4 +30,16 @@ $(document).ready(function () {
 			$this.text('Hide Resume');
 		}
     });
+    
+    $(function() {
+		$.getJSON(baseURL, function(data) {
+			$.each(data.entries, function(i, option) {
+                var entryId = option.entryId;
+                $("span[class='entry" + entryId + "']").text(option.entryName);
+                $("p.date.date" + entryId).text(option.releaseDate);
+                $("div[parentId='entry" + entryId + "'] .text").text(option.overlay);
+                $("img[parentId='entry" + entryId + "']").attr('src', '../assets/tiles/' + option.imgSource + '.png');
+			});
+		});
+	});
 });
