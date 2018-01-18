7a997629012c22a4f3f1e45da94ab92f131475f0
Jacob Leighty <JacobAlexander.Leighty1@T-Mobile.com>
2018-01-18 14:24:03 -0800

    changed font, logo changes on hover, changed diamonds based on content

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,8 +1,26 @@
-var baseURL = 'https://api.myjson.com/bins/1b5m0r';
+var baseURL = 'https://api.myjson.com/bins/1b16m5';
 
 $(document).ready(function () {
     "use strict";
     
+    // Loads JSON file info into featured entries boxes on Front Page
+    $(function() {
+		$.getJSON(baseURL, function(data) {
+			$.each(data.entries, function(i, option) {
+                var entryId = option.entryId;
+                $("span[class='entry" + entryId + "']").text(option.entryName);
+                $("p.date.date" + entryId).text(option.releaseDate);
+                $("div[parentId='entry" + entryId + "'] .text").text(option.overlay);
+                $("img[parentId='entry" + entryId + "']").attr('src', '../assets/tiles/' + option.imgSource + '.png');
+                if (option.entryType === "ux") {
+                    console.log($("img[class='diamonds" + entryId + "']"));
+                    $("img[parentId='" + entryId + "']").attr('src', '../assets/diamond-08.png');
+                }
+			});
+		});
+	});
+    
+    //Handles hover over featured entries on Front Page
     $(document).on("mouseover", ".buttonDiv", function(e) {
         e.preventDefault(); 
         var id = $(this).attr("id");
@@ -18,7 +36,10 @@ $(document).ready(function () {
         $("div[parentId='" + id + "']").css("background", "rgba(0,140,186,0)");
         $("div[parentId='" + id + "'] .text").css("color", "rgba(255,255,255,0)");
     });
+
     
+    
+    // toggles resume visibility on About
     $(document).on("click", "#resumeButton", function(e) {
         e.preventDefault();  
         $(".resumeRow").toggle();
@@ -29,17 +50,5 @@ $(document).ready(function () {
 		} else {
 			$this.text('Hide Resume');
 		}
-    });
-    
-    $(function() {
-		$.getJSON(baseURL, function(data) {
-			$.each(data.entries, function(i, option) {
-                var entryId = option.entryId;
-                $("span[class='entry" + entryId + "']").text(option.entryName);
-                $("p.date.date" + entryId).text(option.releaseDate);
-                $("div[parentId='entry" + entryId + "'] .text").text(option.overlay);
-                $("img[parentId='entry" + entryId + "']").attr('src', '../assets/tiles/' + option.imgSource + '.png');
-			});
-		});
-	});
+    });    
 });
