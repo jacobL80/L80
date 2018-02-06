739b6f15fd2bdfe5954dbb3dd578486eb2de45b1
Jacob Leighty <JacobAlexander.Leighty1@T-Mobile.com>
2018-02-05 16:23:42 -0800

    converted assets into CSS objects

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,5 +1,5 @@
-//var baseURL = 'http://l80.io/assets/frontpage.json';
-var baseURL = 'https://api.myjson.com/bins/gugs1'; // For testing
+var baseURL = 'http://l80.io/assets/frontpage.json';
+//var baseURL = 'https://api.myjson.com/bins/gugs1'; // For testing
 
 $(document).ready(function () {
     "use strict";
@@ -8,29 +8,24 @@ $(document).ready(function () {
     
     // Loads JSON file info into featured entries boxes on Front Page
     $.getJSON(baseURL, function(data) {
-    var entryCounter = 0;
-    $.each(data.entries, function(i, option) {
-        var entryId = option.entryId;
-        var diamonds;
-        if (option.entryType === "ux") {
-            diamonds = "diamond-08.png";
-        } else {
-            diamonds = "diamond-07.png";
-        }
-        if (entryCounter < 10) {
-            $('<div class="buttonDiv" id="' + entryId + '" type="' + option.entryType + '"><img src="assets/tiles/' + option.imgSource + '.png" class="img-responsive buttonImage" parentId="' + entryId + '"/><div class="overlay" parentId="' + entryId + '"><div class="text">' + option.overlay + '</div></div><div class="diamondRow"><img src="assets/' + diamonds + '" class="img-responsive diamonds"/></div><button id="' + entryId + 'Button" class="btn btn-large btn-primary sectionButton" name="Continue" type="button"><span class="' + entryId + '">' + option.entryName + '</span><p class="date">' + option.releaseDate + '</p><i class="icon-ok" style="font-size:30px; vertical-align: middle;"></i></button></div>').appendTo('.contentRowInner');
-            $('<div modalNum="' + entryId + '" id="modal' + entryId + '" class="modal"><div class="container modal-content"><div class="row modalRow" style="height:7%"><span class="close">&times;</span><p class="col-sm-12 modalText">' + option.entryName + '</p></div><div class="row modalRow" style="height:93%"><div class="modalBlock" id="modalBlock' + entryId + '"></div></div></div></div>').appendTo('body');
-        } else {
-            $('<div class="buttonDiv hidden" id="' + entryId + '" type="' + option.entryType + '"><img src="assets/tiles/' + option.imgSource + '.png" class="img-responsive buttonImage" parentId="' + entryId + '"/><div class="overlay" parentId="' + entryId + '"><div class="text">' + option.overlay + '</div></div><div class="diamondRow"><img src="assets/' + diamonds + '" class="img-responsive diamonds"/></div><button id="' + entryId + 'Button" class="btn btn-large btn-primary sectionButton" name="Continue" type="button"><span class="' + entryId + '">' + option.entryName + '</span><p class="date">' + option.releaseDate + '</p><i class="icon-ok" style="font-size:30px; vertical-align: middle;"></i></button></div><div modalNum="' + entryId + '" id="modal' + entryId + '" class="modal"><div class="container modal-content"><div class="row modalRow" style="height:7%"><span class="close">&times;</span><p class="col-sm-12 modalText">' + option.entryName + '</p></div><div class="row modalRow" style="height:93%"><div class="modalBlock" id="modalBlock' + entryId + '"></div></div></div></div>').appendTo('.contentRowInner');
-        }
-        entryCounter++;
+        var entryCounter = 0;
+        $.each(data.entries, function(i, option) {
+            var entryId = option.entryId;
+            var diamonds;
+            if (option.entryType === "ux") {
+                diamonds = "diamondContentL";
+            } else {
+                diamonds = "diamondContentL diamondNav2";
+            }
+            if (entryCounter < 10) {
+                $('<div class="buttonDiv" id="' + entryId + '" type="' + option.entryType + '"><img src="assets/tiles/' + option.imgSource + '.png" class="img-responsive buttonImage" parentId="' + entryId + '"/><div class="overlay" parentId="' + entryId + '"><div class="text">' + option.overlay + '</div></div>    <div class="diamondRow"><div class="diamondRowInner"><div class="diamond diamondContent"></div><div class="diamond diamondNav diamondContent ' + diamonds + '"></div><div class="diamond diamondContent"></div></div>   </div><button id="' + entryId + 'Button" class="btn btn-large btn-primary sectionButton" name="Continue" type="button"><span class="' + entryId + '">' + option.entryName + '</span><p class="date">' + option.releaseDate + '</p><i class="icon-ok" style="font-size:30px; vertical-align: middle;"></i></button></div>').appendTo('.contentRowInner');
+                $('<div modalNum="' + entryId + '" id="modal' + entryId + '" class="modal"><div class="container modal-content"><div class="row modalRow text-center" style="height:7%"><span class="close">&times;</span><p class="col-sm-12 modalText">' + option.entryName + '</p></div><div class="row modalRow" style="height:93%"><div class="modalBlock" id="modalBlock' + entryId + '"></div></div></div></div>').appendTo('body');
+            } else {
+                $('<div class="buttonDiv hidden" id="' + entryId + '" type="' + option.entryType + '"><img src="assets/tiles/' + option.imgSource + '.png" class="img-responsive buttonImage" parentId="' + entryId + '"/><div class="overlay" parentId="' + entryId + '"><div class="text">' + option.overlay + '</div></div>     <div class="diamondRow"><div class="diamondRowInner"><div class="diamond diamondContent"></div><div class="diamond diamondNav diamondContent ' + diamonds + '"></div><div class="diamond diamondContent"></div></div>    </div><button id="' + entryId + 'Button" class="btn btn-large btn-primary sectionButton" name="Continue" type="button"><span class="' + entryId + '">' + option.entryName + '</span><p class="date">' + option.releaseDate + '</p><i class="icon-ok" style="font-size:30px; vertical-align: middle;"></i></button></div><div modalNum="' + entryId + '" id="modal' + entryId + '" class="modal"><div class="container modal-content"><div class="row modalRow text-center" style="height:7%"><span class="close">&times;</span><p class="col-sm-12 modalText">' + option.entryName + '</p></div><div class="row modalRow" style="height:93%"><div class="modalBlock" id="modalBlock' + entryId + '"></div></div></div></div>').appendTo('.contentRowInner');
+            }
+            entryCounter++;
+        });
     });
-    // Load modals with content
-    /*$(".modal").each(function() {
-        var modalNum = $(this).attr("modalNum"); 
-        $("#modalBlock" + modalNum).load("html/content/" + modalNum + ".html");
-    });*/
-});
 
     
     //After page load, checks URL hash and loads its page if there is one
@@ -47,6 +42,7 @@ $(document).ready(function () {
                 //Threads out edge cases where user is abusing URL
                 if (hash === "ux" || hash === "graphic" || hash === "about") { 
                     $(".navbarLinks").removeClass("selected");
+					$("a[id='" + hash + "']").addClass("selected");
                     $("h2").attr("id", hash);
                 }
                 if (hash === "ux") {
@@ -76,9 +72,10 @@ $(document).ready(function () {
     
     
     // Displays only the 10 newest entries
-    $(document).on("click", "h1", function(e) {
+    $(document).on("click", ".navbar-header", function(e) {
         e.preventDefault();
-        if ($("h2").text() !== "FEATURED ENTRIES") {
+		var id = $("h2").attr("id");
+        if (id === "ux" || id === "graphic" || id === "about") {
             $("h2").attr("id", " ");
             history.replaceState('data', '', 'http://L80.io');
             $("#mainContent").css("overflow-y", "hidden");
@@ -86,7 +83,7 @@ $(document).ready(function () {
             $(".contentRowInner, .aboutContentOuter")
                     .css('opacity', 1)
                     .animate({ paddingTop: '5%', opacity: 0 }, 500, function() {
-                        $("h2").text("FEATURED ENTRIES");
+                        $("h2").text("Featured Entries");
                         $(".aboutContentOuter").addClass("hidden");
                         $(".contentRowOuter").removeClass("hidden");
                         $(".buttonDiv").removeClass("hidden");
@@ -112,41 +109,41 @@ $(document).ready(function () {
     //Handles click events for each menu option. Some are new pages and some are toggles
     $(document).on("click", ".navbarLinks", function(e) {
         e.preventDefault(); 
+       
         var id = $(this).attr("id");
         
         $("#mainContent").css("overflow-y", "hidden");
         // Adds the sliding and fading effect
-        $(".contentRowInner, .aboutContentOuter")
+		
+        if (id === "ux" || id === "graphic" || id === "about") {
+            console.log("test2");
+            $(".contentRowInner, .aboutContentOuter")
                 .css('opacity', 1)
                 .animate({ paddingTop: '5%', opacity: 0 }, 500, function() {
-                    if (id === "ux" || id === "graphic" || id === "about") {
-                        $(".navbarLinks").removeClass("selected");
-                        $("a[id='" + id + "']").addClass("selected"); 
-                        $(".buttonDiv").addClass("hidden");
-                        $(".contentRowOuter").removeClass("hidden");
-                        $(".aboutContentOuter").addClass("hidden");
-                    }
-            
-                    if (id === "ux" || id === "graphic" || id === "about") {
-                        $("h2").attr("id", id);
-                    }
+                    console.log("test");
+                    $(".navbarLinks").removeClass("selected");
+                    $("a[id='" + id + "']").addClass("selected");
+                    $(".buttonDiv").addClass("hidden");
+                    $(".contentRowOuter").removeClass("hidden");
+                    $(".aboutContentOuter").addClass("hidden");
+                    
+                    $("h2").attr("id", id);
+                    history.replaceState('data', '', 'http://L80.io/#' + id);
+                    $(".buttonDiv[type='" + id + "']").removeClass("hidden");
+                    
                     if (id === "ux") {
-                        history.replaceState('data', '', 'http://L80.io/#ux');
                         $("h2").text("UX/UI Design & Coding");
-                        $(".buttonDiv[type='ux']").removeClass("hidden");
                     } else if (id === "graphic") {
-                        history.replaceState('data', '', 'http://L80.io/#graphic');
                         $("h2").text("Graphic Design");
-                        $(".buttonDiv[type='graphic']").removeClass("hidden");
                     } else if (id === "about") {
-                        history.replaceState('data', '', 'http://L80.io/#about');
                         $("h2").text("About");
                         $(".contentRowOuter").addClass("hidden");
                         $(".aboutContentOuter").removeClass("hidden");
-                    } else {
-                        window.open('http://l80comics.com', '_blank');
-                    }
-                });
+                    } 
+            });
+        } else {
+            window.open('http://l80comics.com', '_blank');
+        }
         
         //Fades back in when complete
         $(".contentRowInner, .aboutContentOuter")
@@ -172,7 +169,32 @@ $(document).ready(function () {
         $("div[parentId='" + id + "']").css("background", "rgba(0,140,186,0)");
         $("div[parentId='" + id + "'] .text").css("color", "rgba(255,255,255,0)");
     });
+    
+    $(document).on("mouseover", "#navbarLinks li", function(e) {
+        e.preventDefault(); 
+        $(this).children("a").css("color", "#FFF");
+        $(this).children(".diamond").addClass("hovered");
+    });
 
+    $(document).on("mouseout", "#navbarLinks li", function(e) {
+        e.preventDefault(); 
+        $(this).children("a").css("color", "#E15427");
+        $(this).children(".diamond").removeClass("hovered");
+    });
+    
+    $(document).on("mouseover", ".navbar-header", function(e) {
+        e.preventDefault(); 
+        $("h1").css("color", "#FFF");
+        $(".navbar-header .diamond").addClass("hovered");
+    });
+
+    $(document).on("mouseout", ".navbar-header", function(e) {
+        e.preventDefault(); 
+        $("h1").css("color", "#EFE5D1");
+        $(".navbar-header .diamond").removeClass("hovered");
+    });
+    
+    
     
     
     // toggles resume visibility on About
@@ -195,8 +217,11 @@ $(document).ready(function () {
         e.preventDefault();
         id = $(this).attr("id");
         var modal = $("div[id='modal" + id + "']");
-        $(modal).addClass("visible");
-        $(".modalOverlay").addClass("visible");
+        $(".modalOverlay").fadeIn(500);
+		$(modal).addClass('active');
+		$("#mainContent, #mainNavbar").css("pointer-events", "none");
+		/*$("#mainContent, #mainNavbar").removeClass('blur-out');
+		$("#mainContent, #mainNavbar").addClass('blur-in');*/
         $("#modalBlock" + id).load("html/content/" + id + ".html"); // Moved from doing all at once
         history.replaceState('data', '', 'http://L80.io/#' + id);
     });
@@ -204,8 +229,11 @@ $(document).ready(function () {
     // When the user clicks on <span> (x), close the modal
     $(document).on("click", ".close", function(e) {
         e.preventDefault();
-        $(".modal").removeClass("visible");
-        $(".modalOverlay").removeClass("visible");
+        $(".modal").removeClass('active');
+        $(".modalOverlay").fadeOut(500);
+		$("#mainContent, #mainNavbar").css("pointer-events", "auto");
+		/*$("#mainContent, #mainNavbar").removeClass('blur-in');
+		$("#mainContent, #mainNavbar").addClass('blur-out');*/
         id = 0;
         var id2 = $("h2").attr("id");
         if (id2.length > 1) {
@@ -219,8 +247,11 @@ $(document).ready(function () {
     $(document).click(function(event) {
         //if you click on anything except the modal itself or the "open modal" link, close the modal
         if (!$(event.target).closest(".modal-content, .buttonDiv").length) {
-            $("body").find(".modal").removeClass("visible");
-            $(".modalOverlay").removeClass("visible");
+            $("body").find(".modal").removeClass('active');
+            $(".modalOverlay").fadeOut(500);
+			$("#mainContent, #mainNavbar").css("pointer-events", "auto");
+			/*$("#mainContent, #mainNavbar").removeClass('blur-in');
+			$("#mainContent, #mainNavbar").addClass('blur-out');*/
             var id = $("h2").attr("id");
             if (id.length > 1) {
                 history.replaceState('data', '', 'http://L80.io/#' + id);
